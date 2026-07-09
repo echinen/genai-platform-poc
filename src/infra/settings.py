83b2434d8ed01c19938c0ffdc39a60523c649fa9
Eric Chinen <7997656+echinen@users.noreply.github.com>
2026-07-09 01:56:27 -0300

    add new settings file

---
+++ src/infra/settings.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Application settings."""
+
+    app_name: str = "GenAI Platform"
+
+    gemini_api_key: str
+    gemini_model: str = "gemini-3.5-flash"
+    gemini_url: str = "https://generativelanguage.googleapis.com/v1beta/interactions"
+
+    database_url: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
